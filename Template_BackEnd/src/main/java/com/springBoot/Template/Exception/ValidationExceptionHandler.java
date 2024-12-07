d730b350a61fbcad3276467541fb5ed1dd441b20
Ranjithv88 <ranjithkumar22445588@gmail.com>
2024-12-07 17:29:55 +0530

    add cookies Funtion

--- Template_BackEnd/src/main/java/com/springBoot/Template/Exception/ValidationExceptionHandler.java
+++ Template_BackEnd/src/main/java/com/springBoot/Template/Exception/ValidationExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalException {
+public class ValidationExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exceptionHandler (Exception ex) {
